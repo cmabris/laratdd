57e53a0b8e0e761aa38a1d2adca1a0d205515f64
Carlos M Abrisqueta Valcárcel <cmabris@gmail.com>
2022-10-28 19:14:41 +0200

    Paso 3

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Laravel con TDD
                 </div>
 
                 <div class="links">
--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,26 @@
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('/inicio', function () {
+    return 'Hola Mundo';
+});
+
+Route::get('usuarios', function () {
+    return 'Mostrando listado de los usuarios';
+});
+
+Route::get('usuarios/{id}', function ($id) {
+    return 'Mostrando detalle del usuario ' . $id;
+})->where('id', '[0-9]+');
+
+Route::get('usuarios/nuevo', function () {
+    return 'Creando nuevo usuario';
+});
+
+Route::get('saludo/{name}/{nickname?}', function ($name, $nickname = null) {
+    if ($nickname) {
+        return 'Bienvenido ' . ucfirst($name) . '. Tu apodo es ' . $nickname;
+    } else {
+        return 'Bienvenido ' . ucfirst($name) . '. No tienes apodo.';
+    }
+});
