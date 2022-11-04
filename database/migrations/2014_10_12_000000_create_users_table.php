7118cd29bf031120038aa71deaa01ea4b0a5a27a
Carlos Abrisqueta <carlosmanuel.abrisqueta@murciaeduca.es>
2022-11-04 19:26:40 +0100

    Paso 10

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -18,6 +18,7 @@ class CreateUsersTable extends Migration
             $table->string('name');
             $table->string('email')->unique();
             $table->string('password');
+            $table->string('profession', 100);
             $table->rememberToken();
             $table->timestamps();
         });
