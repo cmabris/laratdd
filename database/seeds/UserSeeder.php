bc8fe909b055e9be07898e060c59b8e678501bfa
Carlos Abrisqueta <carlosmanuel.abrisqueta@murciaeduca.es>
2022-11-04 20:57:50 +0100

    Paso 13

--- database/seeds/UserSeeder.php
+++ database/seeds/UserSeeder.php
@@ -11,10 +11,29 @@ class UserSeeder extends Seeder
      */
     public function run()
     {
+        /*$professions = DB::select('SELECT id FROM professions WHERE title="Desarrollador Back-End"');
+        $professions = DB::select(
+            'SELECT id FROM professions WHERE title=?',
+            ['Desarrollador Back-End']
+        );
+        $profession = DB::table('professions')
+            ->select('id')
+            ->where('title', 'Desarrollador Back-End')
+            ->first();
+        $professionId = DB::table('professions')
+            ->where('title', 'Desarrollador Back-End')
+            ->value('id');
+        $professionId = DB::table('professions')
+            ->whereTitle('Desarrollador Back-End')
+            ->value('id');*/
+
         DB::table('users')->insert([
             'name' => 'Pepe Pérez',
             'email' => 'pepe@mail.es',
             'password' => bcrypt('123456'),
+            'profession_id' => DB::table('professions')
+                ->whereTitle('Desarrollador Back-End')
+                ->value('id')
         ]);
     }
 }
