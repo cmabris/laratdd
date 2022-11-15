78dd79d2c01f07f1c2ead7e8f1be7a202485b3b9
Carlos Abrisqueta <carlosmanuel.abrisqueta@murciaeduca.es>
2022-11-15 19:18:23 +0100

    Paso 15

--- app/User.php
+++ app/User.php
@@ -24,6 +24,11 @@ class User extends Authenticatable
      * @var array
      */
     protected $hidden = [
-        'password', 'remember_token',
+        'password', 'remember_token'
     ];
+
+    public function isAdmin()
+    {
+        return $this->email === 'pepe@mail.es';
+    }
 }
