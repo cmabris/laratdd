d0de958a8a82e071c1bb1ff00dd86793b6946626
Carlos M Abrisqueta Valcárcel <cmabris@gmail.com>
2022-12-02 19:53:12 +0100

    Paso 17

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -2,7 +2,7 @@
 
 namespace App\Http\Controllers;
 
-use App\{Http\Requests\CreateUserRequest, Profession, Skill, User, UserProfile};
+use App\{Http\Requests\CreateUserRequest, Http\Requests\UpdateUserRequest, Profession, Skill, User, UserProfile};
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 
@@ -57,32 +57,9 @@ class UserController extends Controller
         return $this->form('users.edit', $user);
     }
 
-    public function update(User $user)
+    public function update(UpdateUserRequest $request, User $user)
     {
-        $data = request()->validate([
-            'name' => 'required',
-            'email' => 'required|email|unique:users,email,'.$user->id,
-            'password' => '',
-            'role' => '',
-            'bio' => '',
-            'twitter' => '',
-            'profession_id' => '',
-            'skills' => '',
-        ]);
-
-        if ($data['password'] != null) {
-            $data['password'] = bcrypt($data['password']);
-        } else {
-            unset($data['password']);
-        }
-
-        $user->fill($data);
-        $user->role = $data['role'];
-        $user->save();
-
-        $user->profile->update($data);
-
-        $user->skills()->sync($data['skills'] ?? []);
+        $request->updateUser($user);
 
         return redirect()->route('user.show', $user);
     }
